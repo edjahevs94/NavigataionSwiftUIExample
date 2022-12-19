b44f3b936a0e907820ef8251f08b51184799e04a
Edgard <20133252@ealoe.ulima.edu.pe>
2022-12-19 11:54:52 -0500

    Initial Commit

---
+++ navigationExample/navigationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationExampleApp.swift
+//  navigationExample
+//
+//  Created by EdgardVS on 19/12/22.
+//
+
+import SwiftUI
+
+@main
+struct navigationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
